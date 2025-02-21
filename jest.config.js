88800083a8a7a4018d23f266ec4898e3c5cad02b
Matheus Ferreira <matheusferreira@pop-os.localdomain>
2025-02-21 16:25:15 -0300

    Configurações para implementar testes

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '^.+\\.js$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'vue'],
-  testMatch: ['**/*.spec.js'],
-};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), cssInjectedByJsPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+    exclude: ['**/*.css'],
+    server: {
+      deps: {
+        inline: ['vuetify'],
+      },
+    },
+  },
+});
